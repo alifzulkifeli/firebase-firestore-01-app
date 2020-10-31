debf390fe3b860c97b52740e60cff0cb8ee68588
aliff <alifzulkifeli@gmail.com>
2020-11-01 05:03:00 +0800

    create component from class

--- src/components/laptop.js
+++ src/components/laptop.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { db } from '../utils/firebase';
 
 export default function laptop() {
+
+  useEffect(() => {
+    
+    return () => {
+      
+    }
+  }, [])
+
   return (
     <div>
       laptop
