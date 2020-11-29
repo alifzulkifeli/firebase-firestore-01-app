867c033ed986079321d7b2cfbcd5cfdd5bde6ba7
aliff <alifzulkifeli@gmail.com>
2020-11-29 19:56:00 +0800

    update the route

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 // components
 import Home from "./components/index";
 import Header from "./components/header";
-import Footer from "./components/footer";
+// import Footer from "./components/footer";
 import Laptop from "./components/Laptop";
 import Login from "./components/user/Login";
 
